5da8a8778e2aace091e5ad4937ec7908827194e1
Ryan Schmelter <rschmelter33@gmail.com>
2018-01-09 00:19:57 +0000

    Initial commit. Set up Gem

---
+++ lib/Housing_List.rb
@@ -0,0 +1,15 @@
+
+
+module HousingList
+  # Your code goes here...
+end
+
+require 'open-uri'
+require 'pry'
+require 'nokogiri'
+
+
+require_relative "Housing_List/version"
+require_relative "Housing_List/cli"
+require_relative "Housing_List/housing"
+require_relative "Housing_List/scraper"
---
+++ lib/Housing_List/version.rb
@@ -0,0 +1,3 @@
+module HousingList
+  VERSION = "0.1.0"
+end
