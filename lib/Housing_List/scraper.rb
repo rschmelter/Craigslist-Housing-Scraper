768fa99075c87b47a6382584606c40ab5d9f32cb
Ryan Schmelter <rschmelter33@gmail.com>
2018-01-13 00:00:29 +0000

    Still need to parse cities

--- lib/Housing_List/scraper.rb
+++ lib/Housing_List/scraper.rb
@@ -7,7 +7,8 @@ class HousingList::Scraper
   def call
     scrape_states
     state_hash
-    state_city_hash
+    city
+    # state_and_city_hash
   end
 
   def scrape_states
@@ -23,14 +24,71 @@ class HousingList::Scraper
   end
 
   def state_hash
-    @state_city = {}
+    @state_hash = {}
     scrape_states.each do |state|
       state_sym = state.to_sym
-      @state_city[state_sym] = nil
+      @state_hash[state_sym] = []
 
       end
-      @state_city
+      @state_hash
     end
+
+    def city
+      i = 0
+
+      @cities = []
+
+
+      doc = Nokogiri::HTML(open('https://www.craigslist.org/about/sites'))
+      a = doc.css(".colmask").first
+      a.css(".box ul").each do |cities|
+        @cities[i] = cities.text
+        i += 1
+      end
+      @cities
+      binding.pry
+    end
+
+# Need to parse city names
+    # ----------------------------------------------------------------------------------------
+
+    def city_array
+      i = 0
+      e = 0
+      @cities = []
+
+
+      doc = Nokogiri::HTML(open('https://www.craigslist.org/about/sites'))
+      a = doc.css(".colmask").first
+      a.css(".box ul").each do |cities|
+        @cities[i] = cities
+        i += 1
+      end
+      @cities
+
+      # @cities_scraped.each do |group|1
+      #   group.css("li").each do |city|
+      #     @cities << city.text
+      #   end
+      #   @cities
+      #   binding.pry
+      # end
+    end
+
+    def state_and_city_hash
+        keys = state_hash.keys
+
+        i = 0
+        keys.each do |state|
+        city_array[i].css("li").each do |city|
+          state_hash[state] << city.text
+        end
+        i += 1
+      end
+      state_hash
+      binding.pry
+
+  end
 
     def state_city_hash
       i = 0
@@ -47,6 +105,8 @@ class HousingList::Scraper
 # Need to parse the cities so that they are seperate items in an array instead of just one.
 
     end
+
+
 
 
 
