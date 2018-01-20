4633b10ef6400a1456d673b1f2e6c08b3027f3d1
Ryan Schmelter <rschmelter33@gmail.com>
2018-01-20 18:14:47 +0000

    Added conditionals to show_states method in CLI

--- lib/Housing_List/cli.rb
+++ lib/Housing_List/cli.rb
@@ -4,14 +4,32 @@ class HousingList::CLI
     HousingList::Scraper.new.make_states.each do |state|
         state = HousingList::State.new(state)
     end
-      i = 1
-      HousingList::State.all.each do |state|
-      puts "#{i}. #{state.name}"
-      i += 1
-
+    puts "Welcome! To get started, type 'list' to show a list of states and areas with available rentals"
+    valid = false
+    while valid == false
+      input = gets.strip
+      if input.downcase == "list"
+        valid = true
+        i = 1
+        HousingList::State.all.each do |state|
+          puts "#{i}. #{state.name}"
+          i += 1
+        end
+      else
+        puts "Invalid input. Please type 'list' to show states"
+      end
     end
-
-
+    puts "Type the number of the state to see a list of cities with available rentals."
+    valid = false
+    while valid == false
+      input = gets.strip
+      state_number = input.to_i
+      if (1..52).include?(state_number)
+        valid = true
+      else
+        puts "Invalid input. Please choos a number between 1 and 52"
+      end
+    end
   end
 
 
