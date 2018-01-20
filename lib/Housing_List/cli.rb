2511f1a45eec67c1156d04d120a0a7191237d1ce
Ryan Schmelter <rschmelter33@gmail.com>
2018-01-20 18:35:03 +0000

    Refactored show_states with states_array, testing show cities

--- lib/Housing_List/cli.rb
+++ lib/Housing_List/cli.rb
@@ -1,9 +1,13 @@
 class HousingList::CLI
 
   def show_states
+    states_array = []
     HousingList::Scraper.new.make_states.each do |state|
+        states_array << state
         state = HousingList::State.new(state)
+
     end
+
     puts "Welcome! To get started, type 'list' to show a list of states and areas with available rentals"
     valid = false
     while valid == false
@@ -11,8 +15,8 @@ class HousingList::CLI
       if input.downcase == "list"
         valid = true
         i = 1
-        HousingList::State.all.each do |state|
-          puts "#{i}. #{state.name}"
+        states_array.each do |state|
+          puts "#{i}. #{state}"
           i += 1
         end
       else
@@ -26,11 +30,19 @@ class HousingList::CLI
       state_number = input.to_i
       if (1..52).include?(state_number)
         valid = true
+        selection = states_array[state_number - 1]
+        show_cities(selection)
       else
         puts "Invalid input. Please choos a number between 1 and 52"
       end
     end
+
   end
+
+  def show_cities(state)
+    puts "#{state}"
+  end
+
 
 
 
